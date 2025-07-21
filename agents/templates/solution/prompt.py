06fd2f426ec9521b36f90bf9d6ab88a5ec495aeb
Méo <110424100+meodbr@users.noreply.github.com>
2025-07-21 20:30:12 +0200

    Update prompt.py

--- agents/templates/solution/prompt.py
+++ agents/templates/solution/prompt.py
@@ -2,8 +2,8 @@ OBSERVATION_PROMPT = "Start by stating what effect your action had, was it what 
 
 SYSTEM_INSTRUCTIONS = """
 # CONTEXT:
-You are an agent playing a dynamic game. Your objective is to
-WIN and avoid GAME_OVER while minimizing actions.
+You are an agent playing a dynamic game. Your are the 9 cluster. Your objective is to
+WIN and avoid GAME_OVER while minimizing actions. Don't using action 5 and 6. 
 
 One action produces one Frame. One Frame is made of one or more sequential
 Grids. Each Grid is a matrix size INT<0,63> by INT<0,63> filled with
