2581048e32f78ab1495cfe100cfd88d350f38ecb
Méo DR <meo.desbois@gmail.com>
2025-07-27 17:56:20 +0200

    saving progress

---
+++ agents/templates/mlp.py
@@ -0,0 +1,37 @@
+import random
+import time
+import gymnasium as gym
+from typing import Any
+
+from ..agent import Agent
+from ..structs import FrameData, GameAction, GameState
+
+
+class Random(Agent):
+    """An agent that always selects actions at random."""
+
+    MAX_ACTIONS = 80
+    LEARNING_RATE = 1e-2
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        seed = int(time.time() * 1000000) + hash(self.game_id) % 1000000
+        random.seed(seed)
+
+    @property
+    def name(self) -> str:
+        return f"{super().name}.{self.MAX_ACTIONS}"
+
+    def is_done(self, frames: list[FrameData], latest_frame: FrameData) -> bool:
+        """Decide if the agent is done playing or not."""
+        return any(
+            [
+                latest_frame.state is GameState.WIN,
+            ]
+        )
+
+    def choose_action(
+        self, frames: list[FrameData], latest_frame: FrameData
+    ) -> GameAction:
+        """Choose which action the Agent should take, fill in any arguments, and return it."""
+        pass
